b18ebf0586dccb3ea1e0d7d74dbb81785762fa32
adamhockenberry <hockenberry.adam@gmail.com>
2020-01-30 15:34:11 -0600

    developing ete3 based rooting functions

--- Code/rooting_methods_ete3.py
+++ Code/rooting_methods_ete3.py
@@ -1,79 +1,81 @@
 import numpy as np
 
-def MP_root_ete3(tree):
+def MP_root_ete3(my_tree):
     """
     
     """
-    init_bl = np.sum([i.dist for i in tree.traverse()])
-    init_terms = tree.get_leaves()
-    init_term_names = [i.name for i in tree.get_leaves()]
-    if len(tree.children) == 3:
-        tree.set_outgroup(tree.get_leaves()[0])
-    assert set([len(i.children) for i in tree.traverse() if not i.is_leaf()]) == set([2])
+    init_bl = np.sum([i.dist for i in my_tree.traverse()])
+    init_terms = my_tree.get_leaves()
+    init_term_names = [i.name for i in my_tree.get_leaves()]
+    if len(my_tree.children) == 3:
+        my_tree.set_outgroup(my_tree.get_leaves()[0])
+    assert set([len(i.children) for i in my_tree.traverse() if not i.is_leaf()]) == set([2])
     ###Identifying the two leaves that are farthest from one-another
-    starting, trash = tree.get_farthest_leaf()
+    starting, trash = my_tree.get_farthest_leaf()
     farthest1, init_dist = starting.get_farthest_node()
     farthest2, max_dist = farthest1.get_farthest_node()
     assert farthest1.is_leaf() and farthest2.is_leaf()
     
     ###Actually performing the mid-point root
-    final_root(tree, farthest1, farthest2, max_dist)
+    final_root(my_tree, farthest1, farthest2, max_dist)
     
     ###Making sure the two leave are equi-distant from one-another
-    assert np.isclose(tree.get_distance(farthest1), tree.get_distance(farthest2))
+    assert np.isclose(my_tree.get_distance(farthest1), my_tree.get_distance(farthest2))
     ###Making sure that I didn't lose any branch length along the way
-    final_bl = np.sum([i.dist for i in tree.traverse()])
+    final_bl = np.sum([i.dist for i in my_tree.traverse()])
     assert np.isclose(init_bl, final_bl)
     ###Making sure I didn't lose any leaves along the way
-    assert set(init_term_names) == set([i.name for i in tree.get_leaves()])
+    assert set(init_term_names) == set([i.name for i in my_tree.get_leaves()])
     ###And that I'm still fully bifurcating
-    assert set([len(i.children) for i in tree.traverse() if not i.is_leaf()]) == set([2])
+    assert set([len(i.children) for i in my_tree.traverse() if not i.is_leaf()]) == set([2])
     return
 
-def final_root(tree, farthest1, farthest2, max_dist):
-    """
-    This is brutal and needs to be simplified even though it works fine
-    """
-    ###An annoyingly convoluted way to get the path between two nodes, ignoring the dist of the LCA
-    lca = tree.get_common_ancestor(farthest1, farthest2)
-    temp_node = farthest1
-    path1 = []
-    while temp_node != lca:
-        path1.append(temp_node)
+def get_ancestor_path(parent, target):
+    temp_node = target
+    path = []
+    while temp_node != parent:
+        path.append(temp_node)
         temp_node = temp_node.up
-    temp_node = farthest2
-    path2 = []
-    while temp_node != lca:
-        path2.append(temp_node)
-        temp_node = temp_node.up
-    path2 = path2[::-1]
+    return path
+
+def get_shortest_path(my_tree, node1, node2):
+    lca = my_tree.get_common_ancestor(node1, node2)
+    path1 = get_ancestor_path(lca, node1)
+    path2 = get_ancestor_path(lca, node2)
+    path2 = path2[::-1] ###Reverse this list to correctly orient the final path
     path = path1+path2
-    
-    ###Finding the correct branch for the final root
-    counter = 0
-    for i,j in zip([i.dist for i in path], path):
-        counter += i
-        if counter < max_dist/2:
-            old_path = j
-            continue
-        else:
-            remainder = counter - (max_dist/2)
-            counter -= i
-            break
-            
-    ###Rooting the tree accordingly
-    if len(path) > 0:
-        tree.set_outgroup(old_path)
-        if tree.children == [old_path, j]:
-            total = np.sum([i.dist for i in tree.children])
-            tree.children[1].dist = remainder
-            tree.children[0].dist = total-remainder
-        else:
-            tree.set_outgroup(j)
-            total = np.sum([i.dist for i in tree.children])
-            tree.children[0].dist = remainder
-            tree.children[1].dist = total-remainder
-    else:
-        tree.set_outgroup(farthest2)
-    return
+    return path
 
+def final_root(my_tree, farthest1, farthest2, max_dist):
+    """
+    """
+    path = get_shortest_path(my_tree, farthest1, farthest2)
+    assert np.isclose(np.sum([i.dist for i in path]), max_dist)
+    ###Finding the correct branch for the final root
+    mid_point = max_dist/2.
+    counter = 0
+    for node in path:
+        counter += node.dist
+        if counter < mid_point:
+            last_success = node
+        else:
+            remainder = counter - mid_point
+            break        
+    ###Rooting the my_tree accordingly
+    my_tree.set_outgroup(node)
+    ###Checking the orientation (asking whether the other node is indeed a child)
+    if my_tree.children[0] != node:
+        my_tree.swap_children()
+    ###If the topology doesn't get deformed by the rooting
+    if my_tree.children == [node, last_success]:
+        total = np.sum([i.dist for i in my_tree.children])
+        my_tree.children[0].dist = remainder
+        my_tree.children[1].dist = total-remainder
+    ###And if it does induce a topology deformation
+    else:
+        assert last_success in my_tree.children[1].children        
+        total = np.sum([i.dist for i in my_tree.children + [last_success]])
+        my_tree.children[0].dist = remainder
+        my_tree.children[1].dist = 0.
+        last_success.dist = total-remainder
+    return
